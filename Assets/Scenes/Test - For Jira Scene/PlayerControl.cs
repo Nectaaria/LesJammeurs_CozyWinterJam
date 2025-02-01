bd7ce34ccb0d445efae01ff2b61c1d201a804f22
Konayuki3 <jiraknight3@yahoo.com>
2025-02-01 18:21:57 +0100

    Update Speed Controller

--- Assets/Scenes/Test - For Jira Scene/PlayerControl.cs
+++ Assets/Scenes/Test - For Jira Scene/PlayerControl.cs
@@ -1,24 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptPlayer : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public float moveSpeed = 250;
 
-    }
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * 5f * Time.fixedDeltaTime * Input.GetAxis("Vertical"));
-        transform.Translate(Vector3.right * 5f * Time.fixedDeltaTime * Input.GetAxis("Horizontal"));
+        float horizonMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        MovePlayer(horizonMovement);
+    }
+
+    void MovePlayer(float _horizonMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizonMovement, rb.linearVelocity.y);
+        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, .05f);
     }
 }
